a4f364bb4f820f03c80528478bab91921a5c2af0
ShofarEl <138009554+ShofarEl@users.noreply.github.com>
2025-08-29 07:44:19 +0100

    Update auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -100,3 +100,15 @@ export const protect = async (req, res, next) => {
     });
   }
 };
+
+// Grant access to specific roles
+export const authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        message: `User role ${req.user.role} is not authorized to access this route`
+      });
+    }
+    next();
+  };
+};
